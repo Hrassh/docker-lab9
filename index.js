251efab0a5c551f27834da9c8cbca2b5fdbcfac1
Hrassh <tema33step@gmail.com>
2026-09-24 11:44:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello world from Docker!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
